c6fd0ca5f47e39d950c5cb2b37a4b29964bfa013
Tyler Cox <ktylerc92@gmail.com>
2017-10-28 04:07:08 -0400

    Reverted map changed

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,1 +1,1 @@
-./srcds_run -game csgo -console -usercon +ip 0.0.0.0 -port 27020 +map de_dust2 +game_type 0 +game_mode 1 -tickrate 84 -autoupdate
+./srcds_run -game csgo -console -usercon +ip 0.0.0.0 -port 27020 +map surf_utopia_night +game_type 0 +game_mode 1 -tickrate 84 -autoupdate
